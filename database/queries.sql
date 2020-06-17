5053c54978c87b0bd250b09d77dfbcb65062cda3
Sasha <alexandra.oswald@uol.de>
2020-06-17 15:41:17 +0200

    Fehler im Skript

--- database/queries.sql
+++ database/queries.sql
@@ -16,16 +16,16 @@ insert into jobangebot (user_id, status) VALUES (1,0);
 insert into jobangebot_details (jobangebot_id, im_bachelor, im_master, bachelor, master, ausbildung) VALUES (1,0,0,0,0,0);
 
 select * from user;
---OUTPUT:
---id|vname|nname|password|mail|uname|strasse|hausnr|plz|verified|mail_verified
---1|test|test|12345678|test@gmail.com|NULL|NULL|NULL|NULL|0|0
+-- OUTPUT:
+-- id|vname|nname|password|mail|uname|strasse|hausnr|plz|verified|mail_verified
+-- 1|test|test|12345678|test@gmail.com|NULL|NULL|NULL|NULL|0|0
 
 select * from jobangebot;
---OUTPUT:
---id|user_id|status|titel|strasse|hausnr|plz|beschreibung
---1|1|0|NULL|NULL|NULL|NULL|NULL
+-- OUTPUT:
+-- id|user_id|status|titel|strasse|hausnr|plz|beschreibung
+-- 1|1|0|NULL|NULL|NULL|NULL|NULL
 
 select * from jobangebot_details;
---OUTPUT:
---id|jobangebot_id|art|im_bachelor|bachelor|im_master|master|ausbildung
---1|1|NULL|0|0|0|0|0
+-- OUTPUT:
+-- id|jobangebot_id|art|im_bachelor|bachelor|im_master|master|ausbildung
+-- 1|1|NULL|0|0|0|0|0
