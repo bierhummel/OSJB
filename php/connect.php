12a997baa7c4d6d19fc0013c80d681f818348869
Sasha <alexandra.oswald@uol.de>
2020-06-18 14:10:57 +0200

    Ein bisschen hier von, ein bisschen da von
    Minor changes

--- php/connect.php
+++ php/connect.php
@@ -1,12 +1,25 @@
 <?php 
-$database = "../database/database.db";
 
+class Connection
+{
+private $database = "../database/database.db";
+private $db;
+
+public function connect(){
 if (!file_exists($datenbank)) {
- $db = new PDO('sqlite:' . $database);
- $db->exec("create table user (id integer primary key, uname text NOT NULL, vname text NOT NULL, nname text NOT NULL, password text NOT NULL, mail text NOT NULL, strasse text, hausnr text, plz text, verified integer NOT NULL, mail_verified integer NOT NULL)");
-  $db->exec("create table jobangebot (id integer primary key, user_id integer, status integer NOT NULL, titel text, strasse text, hausnr text, plz text, beschreibung text, art text, im_bachelor integer NOT NULL, bachelor integer NOT NULL, im_master integer NOT NULL, master integer NOT NULL, ausbildung integer NOT NULL, fachrichtung text, logo blob, link text, beschaeftigungsbeginn text, FOREIGN KEY (user_id) REFERENCES user(id))");}
+ this->$db = new PDO('sqlite:' . $database);
+ this->$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ try{    
+ this->$db->exec("create table user (id integer primary key, uname text NOT NULL, vname text NOT NULL, nname text NOT NULL, password text NOT NULL, mail text NOT NULL, strasse text, hausnr text, plz text, verified integer NOT NULL, mail_verified integer NOT NULL)");
+ this->$db->exec("create table jobangebot (id integer primary key, user_id integer, status integer NOT NULL, titel text, strasse text, hausnr text, plz text, beschreibung text, art text, im_bachelor integer NOT NULL, bachelor integer NOT NULL, im_master integer NOT NULL, master integer NOT NULL, ausbildung integer NOT NULL, fachrichtung text, logo blob, link text, beschaeftigungsbeginn text, FOREIGN KEY (user_id) REFERENCES user(id))");}
+    catch(PDOException $e) {
+    // Zeige PDOException message
+    echo $e->getMessage();
+  }
+}
 else {
- $db = new PDO('sqlite:' . $database);
+ this->$db = new PDO('sqlite:' . $database);
+ this->$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
 }    
 // Überprüfen der Rechte
 if (!is_writable($database)) {
@@ -16,7 +29,11 @@ if (!is_writable($database)) {
 // Fehlermeldungen
 error_reporting(E_ALL);
 ini_set('display_errors', true);        
-
-
+}
+    
+public function getDatabase(){
+        return this->$db;
+    }
+}
 
 ?>
