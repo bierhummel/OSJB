0e6f7d4b87925414c80f04d5dfec5d764630f070
ejanssen <ejanssen@ewe.net>
2020-07-08 04:02:24 +0200

    Datei für Nutzungsbedingungen erstellt, Muster eingefügt (nicht fertig)
    Text muss noch an unsere Website angepasst werden

---
+++ nutzungsbedingungen.php
@@ -0,0 +1,90 @@
+<?php
+
+ini_set("session.use_cookies", 1); 
+ini_set("session.use_only_cookies", 0);
+ini_set("session.use_trans_sid", 1);
+
+session_start();
+?>
+
+<!DOCTYPE HTML>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" type="image/x-icon" href="bilder/favicon-16x16.png">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>OSJB - Nutzungsbedingungen</title>
+</head>
+
+<body>
+
+    <?php
+        $title = "OSJB";
+        include "php/header.php";
+    ?>
+
+    <div class="container border">
+
+        <section>
+            <h1 class="center">Nutzungsbedingungen</h1>
+
+            <p><strong>§ 1 Geltungsbereich</strong></p>
+            <p>Für die Nutzung der Website OSJB von OSJB e.V. (nachfolgend "Anbieter" genannt) gelten nachfolgende Bedingungen. </p>
+            <p>Die Nutzung der Website ist nur zulässig, wenn Sie als Nutzer diese Nutzungsbedingungen akzeptieren. </p>
+            <p><strong>§ 2 Registrierung, Vertragsschluss und -gegenstand</strong></p>
+            <p>1. Voraussetzung für die Nutzung der Website ist die Registrierung über das entsprechende Online-Formular. Nach der Registrierung über das Online-Formular im Forum bekommen Sie eine Bestätigungsemail zur Verifizierung Ihrer Daten zugeschickt, mit der Sie Ihre Registrierung per Mouseclick bestätigen können. Mit der Aktivierung Ihres Accounts durch den Anbieter, kommt der unentgeltliche Foren-Nutzungsvertrag zustande (Vertragsschluss). </p>
+            <p>2. Vertragsgegenstand ist die kostenlose Nutzung der Funktionen der Website als Online-Kommunikationsplattform. Hierzu wird Ihnen als Nutzer ein „Account“ bereitgestellt, mit dem Sie Beiträge und Themen im Forum einstellen können.</p>
+            <p>3. Es gibt grundsätzlich keinen Rechtsanspruch auf Freischaltung oder Teilnahme am Forum. Es gilt das uneingeschränkte Hausrecht des Betreibers.</p>
+            <p>4. Ihr Foren-Account darf nur von Ihnen selbst genutzt werden. Ebenso sind Sie als Inhaber des Accounts für den Schutz vor dessen Missbrauch verantwortlich. Ihre Zugangsdaten sind daher vor dem Zugriff Dritter zu schützen. Die Verwendung von markenrechtlich geschützten Worten und Internetadressen als Benutzername (Nickname) sind nicht erlaubt.</p>
+            <p>5. Der Anbieter wird sich bemühen, den Dienst möglichst unterbrechungsfrei zum Abruf anzubieten. Auch bei aller Sorgfalt können Ausfallzeiten nicht ausgeschlossen werden, in denen die Webserver auf Grund von technischen oder sonstigen Problemen, die nicht im Einflussbereich vom Anbieter liegen (Verschulden Dritter, höhere Gewalt, Angriffe gegen die Infrastruktur durch Hacker etc.), über das Internet nicht abrufbar ist. Der Nutzer erkennt an, dass eine 100%ige Verfügbarkeit der Website technisch nicht zu realisieren ist.</p>
+            <p>6. Der Anbieter behält sich das Recht vor, Inhalt und Struktur der Plattform sowie die dazugehörigen Benutzeroberflächen zu ändern und zu erweitern, wenn hierdurch die Zweckerfüllung des mit dem Nutzer geschlossenen Vertrags nicht oder nur unerheblich beeinträchtigt wird. Der Anbieter wird die Nutzer entsprechend über die Änderungen informieren.</p>
+            <p>7. Sinn und Zweck der Website ist ein an die Öffentlichkeit gerichteter "Markt der Meinungen". Es soll daher unter den Nutzern ein friedlicher und respektvoller Umgang ohne beleidigende Anfeindungen gepflegt werden.</p>
+            <p><strong>§ 3 Pflichten als Foren-Nutzer</strong></p>
+            <p>1. Als Nutzer verpflichten Sie sich, dass Sie keine Beiträge veröffentlichen werden, die gegen diese Regeln, die guten Sitten oder sonst gegen geltendes deutsches Recht verstoßen. Es ist Ihnen insbesondere untersagt,</p>
+            <ul>
+                <li>beleidigende oder unwahre Inhalte zu veröffentlichen;</li>
+                <li>Spam über das System an andere Nutzer zu versenden;</li>
+                <li>gesetzlich, insbesondere durch das Urheber- und Markenrecht, geschützte Inhalte ohne Berechtigung zu verwenden;</li>
+                <li>wettbewerbswidrige Handlungen vorzunehmen; </li>
+                <li>Ihr Thema mehrfach im Forum einzustellen (Verbot von Doppelpostings);</li>
+                <li>Presseartikel Dritter ohne Zustimmung des Urhebers im Forum zu veröffentlichen;</li>
+                <li>Werbung im Forum ohne ausdrückliche schriftliche Genehmigung durch den Anbieter zu betreiben. Dies gilt auch für sog. Schleichwerbung wie insbesondere das Verlinken der eigenen Homepage mit oder ohne Beitext in der Signatur oder innerhalb von Beiträgen. Homepage-URLs und Adress- bzw. Kontaktdaten dürfen nur im Benutzer-Profil der Website veröffentlicht werden.</li>
+            </ul>
+            <p>2. Als Nutzer verpflichten Sie sich, vor der Veröffentlichung Ihrer Beiträge und Themen diese daraufhin zu überprüfen, ob diese Angaben enthalten, die Sie nicht veröffentlichen möchten. Ihre Beiträge und Themen können in Suchmaschinen erfasst und damit weltweit zugreifbar werden. Ein Anspruch auf Löschung oder Korrektur solcher Suchmaschineneinträge gegenüber dem Anbieter ist ausgeschlossen.</p>
+            <p>3. Beim Verstoß, insbesondere gegen die zuvor genannten Regeln § 3 Abs. 1 und 2, kann der Anbieter unabhängig von einer Kündigung, auch die folgenden Sanktionen gegen den Nutzer verhängen:</p>
+            <ul>
+                <li>Löschung oder Abänderung von Inhalten, die der Nutzer eingestellt hat,</li>
+                <li>Ausspruch einer Abmahnung oder</li>
+                <li>Sperrung des Zugangs zum Forum.</li>
+            </ul>
+            <p>4. Sollten Dritte oder andere Nutzer den Anbieter wegen möglicher Rechtsverstöße in Anspruch nehmen, die a) aus den von Ihnen als Nutzer eingestellten Inhalten resultieren und/oder b) aus der Nutzung der Dienste des Anbieters durch Sie als Nutzer entstehen, verpflichten Sie sich als Nutzer, den Anbieter von jeglichen Ansprüchen, einschließlich Schadensersatzansprüchen, freizustellen und dem Anbieter die Kosten zu ersetzen, die diesem wegen der möglichen Rechtsverletzung entstehen. Der Anbieter wird insbesondere von den Kosten der notwendigen Rechtsverteidigung freigestellt. Der Anbieter ist berechtigt, hierfür von Ihnen als Nutzer einen angemessenen Vorschuss zu fordern. Als Nutzer sind Sie verpflichtet, den Anbieter nach Treu und Glauben mit Informationen und Unterlagen bei der Rechtsverteidigung gegenüber Dritten zu unterstützen. Alle weitergehenden Rechte sowie Schadensersatzansprüche des Anbieters bleiben unberührt. Wenn Sie als Nutzer die mögliche Rechtsverletzung nicht zu vertreten haben, bestehen die zuvor genannten Pflichten nicht. </p>
+            <p><strong>§ 4 Übertragung von Nutzungsrechten</strong></p>
+            <p>1. Das Urheberrecht für Ihre Themen und Beiträge, soweit diese urheberrechtsschutzfähig sind, verbleibt grundsätzlich bei Ihnen als Nutzer. Sie räumen dem Anbieter jedoch mit Einstellung eines Themas oder Beitrags das Recht ein, das Thema oder den Beitrag dauerhaft auf seinen Webseiten vorzuhalten. Zudem hat der Anbieter das Recht, Ihre Themen und Beiträge zu löschen, zu bearbeiten, zu verschieben oder zu schließen.</p>
+            <p>2. Die zuvor genannten Nutzungsrechte bleiben auch im Falle einer Kündigung des Foren-Accounts bestehen. </p>
+            <p><strong>§ 5 Haftungsbeschränkung</strong></p>
+            <p>1. Der Anbieter der Website übernimmt keinerlei Gewähr für die im Forum eingestellten Inhalte, insbesondere nicht für deren Richtigkeit, Vollständigkeit und Aktualität. </p>
+            <p>2. Der Anbieter haftet für Vorsatz und grobe Fahrlässigkeit sowie bei Verletzung einer wesentlichen Vertragspflicht. Wesentliche Vertragspflichten sind solche, deren Erfüllung die ordnungsgemäße Durchführung des Vertrags überhaupt erst ermöglicht und auf deren Einhaltung der Vertragspartner regelmäßig vertrauen darf. Der Anbieter haftet unter Begrenzung auf Ersatz des bei Vertragsschluss vorhersehbaren vertragstypischen Schadens für solche Schäden, die auf einer leicht fahrlässigen Verletzung von wesentlichen Vertragspflichten durch ihn oder eines seiner gesetzlichen Vertreter oder Erfüllungsgehilfen beruhen. Bei leicht fahrlässiger Verletzung von Nebenpflichten, die keine wesentlichen Vertragspflichten sind, haftet der Anbieter nicht. Die Haftung für Schäden, die in den Schutzbereich einer vom Anbieter gegebenen Garantie oder Zusicherung fallen sowie die Haftung für Ansprüche aufgrund des Produkthaftungsgesetzes und Schäden aus der Verletzung des Lebens, des Körpers oder der Gesundheit bleibt hiervon unberührt.</p>
+            <p><strong>§ 6</strong><strong> Laufzeit / Beendigung des Vertrags</strong></p>
+            <p>1. Diese Vereinbarung wird auf unbestimmte Zeit geschlossen.</p>
+            <p>2. Beide Parteien können diese Vereinbarung ohne Einhaltung einer Frist kündigen.</p>
+            <p>3. Wenn der Nutzer seinen Account löscht bzw. löschen lässt (Kündigung des Vertrags), bleiben seine öffentlichen Äußerungen, insbesondere Beiträge im Forum, weiterhin für alle Leser sichtbar, der Account ist jedoch nicht mehr abrufbar und im Forum mit „Gast“ gekennzeichnet. Alle anderen Daten werden gelöscht. Wenn der Nutzer wünscht, dass auch seine öffentlichen Beiträge gelöscht werden, möge er dies beim Löschwunsch dem Anbieter mitteilen.</p>
+            <p><strong>Quelle: <a href="https://www.juraforum.de/muster-nutzungsbedingungen-forum/" target="_blank" title="Forenregeln">Forenregeln Muster</a> von <a href="https://www.juraforum.de/" target="_blank" title="Juraforum">Juraforum.de</a></strong></p>
+
+
+        </section>
+
+    </div>
+
+    <?php
+        include "php/footer.php";
+    ?>
+
+</body>
+
+</html>
