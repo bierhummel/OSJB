bb8767b87b54353f0a0025597a5da1b9dc86c51f
ejanssen <ejanssen@ewe.net>
2020-07-08 04:03:44 +0200

    Fußzeile angepasst mit Links zu den 3 neuen Datein

--- php/footer.php
+++ php/footer.php
@@ -2,21 +2,25 @@
     <hr>
     <nav class="container-fluid" id="footer">
         <div class="row">
-            <div class="col-md-4">
-                <a href="impr-agb-ds.php" class="btn btn-link">AGB - Impressum - Datenschutz</a>
+            <div class="col-md-2 ">
+                <a href="impressum.php" class="btn btn-link">Impressum</a>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-2 ">
+                <a href="datenschutz.php" class="btn btn-link">Datenschutz</a>
+            </div>
+            <div class="col-md-3 ">
+                <a href="nutzungsbedingungen.php" class="btn btn-link">Nutzungsbedingungen</a>
+            </div>
+
+
+            <div class="col-md-3 ">
                 <a href="about.php" class="btn btn-link">Über uns</a>
             </div>
-            <div class="col-md-4">
-                
-                <?php if ( isset ($_SESSION["eingeloggt"]) && $_SESSION["eingeloggt"] == "true" ) { ?>                                    
+            <div class="col-md-2 ">
+                <?php if ( isset ($_SESSION["eingeloggt"]) && $_SESSION["eingeloggt"] == "true" ) { ?>
                     <a href="php/controller-logout.php" class="btn btn-link">Abmelden</a>
-                
                 <?php } else { ?>
-                
                     <a href="login.php" class="btn btn-link">Anmelden</a>
-                
                 <?php } ?>
             </div>        
         </div>
