d4ea05caaba72ac13a89f05f60942a34377fa2a1
ejanssen <ejanssen@ewe.net>
2020-07-29 19:57:55 +0200

    Header überarbeitet
    -Zeigt nun den Button aus Index oben rechts an wenn nicht eingeloggt oder ein Button für Profil und einen zum Ausloggen wenn eingeloggt
    -Dafür bootstrap grid eingebaut
    -Aufgeräumt (alten Header und anderen alten Code rausgeworfen, kommentare eingefügt, etc.)

--- php/header.php
+++ php/header.php
@@ -1,32 +1,27 @@
 <header class="container-fluid jumbotron">
-    <h1 class="center">
-        <a href="index.php">
-            OSJB
-        </a>        
-        <!--<?=$title?>-->
-    </h1>
+    <div class="row">
+        <!--leeres div für Symetrie-->
+        <div class="col-md-4">
+        </div>
+        <!--Schriftzug der immer zum Index verlinkt-->
+        <div class="col-md-4 ">
+            <h1 class="center">
+                <a href="index.php">OSJB</a>
+            </h1>    
+        </div>
+        <!--Links für Login/Logout + ggf. zum Profil-->
+        <div class="col-md-4">
+            <h2 class="login-links">
+                <?php if( isset($_SESSION["eingeloggt"]) && $_SESSION["eingeloggt"] == "true" ) { ?>
+                    <a href="profil.php" class="btn btn-primary login-links mr-2">Profil</a>
+                    
+                    <a href="php/controller-logout.php" class="btn btn-secondary login-links">Abmelden</a>
+                <?php } else { ?>
+                    <a href="login.php" class="btn btn-primary login-links">Anmeldung/Registrierung für Arbeitgeber</a>
+                <?php } ?>
+            </h2>
+        </div>
+    </div>
 </header>
 
 <!--Behandlung von unerwünschten Aufrufen?-->
-
-<!--alter Header Übergangsweise noch abgespeichert für eventuelle spätere Zwecke-->
-<!--
-<div class="container-fluid jumbotron">
-    <header>
-        <div class="row">
-            <div class="col-1">
-                <nav>
-                    <a href="index.html">
-                        <img src="bilder/back_button.png" alt="Zurück" width="50" height="50">
-                    </a>
-                </nav>
-            </div>
-            <div class="col-11">
-                <h1 class="center">
-                    Impressum, AGB und Datenschutz
-                </h1><br>
-            </div>
-        </div>
-    </header>
-</div>
--->
